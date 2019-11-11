4abeae55488531c35f4cf099a47d32d2a4d7bb69
Navarro2111 <manpastor2@hotmail.com>
2019-11-11 13:22:04 +0100

    Commit con el metodo iniciar sesion.

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,18 +9,31 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
-		iniciarSesion("Contraseñas.txt");
+		String usuario="Francisco";
+		String contraseña="Francisco";
+		iniciarSesion(usuario,contraseña);
 
 	}
 
-	private static void iniciarSesion(String archivo) {
+	private static boolean iniciarSesion(String usuario, String contraseña) {
 		String cadena;
         FileReader f;
+        boolean permiso=false;
 		try {
+			File archivo = new File ("C:\\Users\\manpa\\Proyecto Ingenieria\\Proyecto Ingenieria\\src\\model\\Contraseñas.txt");
+			
 			f = new FileReader(archivo);
 			BufferedReader b = new BufferedReader(f);
 			while((cadena = b.readLine())!=null) {
-	            System.out.println(cadena);
+				int coma=cadena.indexOf("-");
+				String usuarioB=cadena.substring(0, coma).trim();
+				String contraseñaB=cadena.substring(coma+1).trim();
+				if(usuario.equals(usuarioB)&&contraseña.equals(contraseñaB)){
+					permiso=true;
+				}
+				
+				
+	            
 	        }
 	        b.close();
 		} catch (FileNotFoundException e) {
@@ -29,6 +43,7 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return permiso;
         
         
 		
